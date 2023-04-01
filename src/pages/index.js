92f9b159f50024848bf47cc80359d576c32efe03
XUSdLfXD <devilwork123123@gmail.com>
2023-04-01 21:38:01 +0300

    deleted comments

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,14 +15,3 @@ export default function Home() {
     </>
   );
 }
-
-// export async function getServerSideProps() {
-//   const response = await fetch("http://localhost:3000/api/products");
-//   const { products } = await response.json();
-
-//   return {
-//     props: {
-//       products,
-//     },
-//   };
-// }
