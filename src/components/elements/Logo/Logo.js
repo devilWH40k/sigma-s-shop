82731112d9183f7a4364def5cd3506805976bb6f
XUSdLfXD <devilwork123123@gmail.com>
2023-04-05 00:46:21 +0300

    added priority flag to Logo

--- src/components/elements/Logo/Logo.js
+++ src/components/elements/Logo/Logo.js
@@ -2,7 +2,7 @@ import logo from "@assets/Header/logo.png";
 import Image from "next/image";
 
 const Logo = function () {
-  return <Image src={logo} alt="logo image" width={200} height={54} />;
+  return <Image src={logo} alt="logo image" width={200} height={54} priority />;
 };
 
 export default Logo;
