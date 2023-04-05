682a382114215ef533a99e8ff651025b81e104a2
XUSdLfXD <devilwork123123@gmail.com>
2023-04-05 04:44:29 +0300

    edited Loader and added to persist gate

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import "@/styles/globals.scss";
+import Loader from "@/components/elements/Loader/Loader";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import store, { persistor } from "@/store/store";
@@ -6,7 +7,7 @@ import store, { persistor } from "@/store/store";
 export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+      <PersistGate loading={<Loader />} persistor={persistor}>
         <Component {...pageProps} />
       </PersistGate>
     </Provider>
